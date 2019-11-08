b3502bdd263c2c172f97a37ec400e1a442d8233c
itjeon <itjeon@inspace.re.kr>
2019-11-08 18:03:00 +0900

    initial commit

---
+++ src/main/java/com/example/demo/web/WebRestController.java
@@ -0,0 +1,27 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.posts.PostsRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+	
+	private PostsRepository postsRepository;
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World";
+	}
+	
+	@PostMapping("posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+		postsRepository.save(dto.toEntity());
+	}
+}
